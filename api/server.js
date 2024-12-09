5c3bf6326db2f6a1c99d026157fb5c5bd533ca28
yurenianastya <nastyayurenia@gmail.com>
2024-12-09 17:01:33 +0100

    testing

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const jsonServer = require('json-server');
+const path = require('path');
+const cors = require('cors');
+
 const server = jsonServer.create();
-const router = jsonServer.router('../public/data/db.json');
+const router = jsonServer.router(path.join(__dirname, '../public/data/db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use('/api', router);
+server.use(cors());
 
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running at http://localhost:${PORT}`);
+});
 module.exports = server;
